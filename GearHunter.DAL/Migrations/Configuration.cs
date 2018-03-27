336c34b0a4268b07b70c0cda3e1e6984379237f8
Stefan Krabbe <stefankrabbe54@gmail.com>
2018-03-27 17:26:44 +0200

    Added Faker.Net and NBuilder and tried to make it work, but it doesnt work yet

--- GearHunter.DAL/Migrations/Configuration.cs
+++ GearHunter.DAL/Migrations/Configuration.cs
@@ -1,8 +1,10 @@
-using GearHunter.Core.Models;
+using GearHunter.Core;
 
 namespace GearHunter.DAL.Migrations
 {
+    using FizzWare.NBuilder;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -14,19 +16,28 @@ namespace GearHunter.DAL.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(GearHunter.DAL.GearHunterDbContext context)
+        protected override void Seed(GearHunterDbContext context)
         {
-            Category category = new Category() {Name = "Guitar"};
-            context.Categories.AddOrUpdate(x => x.Id, category);
-
-            context.Advertisements.AddOrUpdate( x => x.Id,
-                new Advertisement("Smart guitar", "Maerke1", "Model1", 1, "D1", "A1", "9000", "Aalborg", true, true, true, DateTime.Now, null, category)
-                );
-
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+
+            //var individuals = Builder<Individual>.CreateListOfSize(100)
+            //    .All()
+            //    .With(x => x.Name = Faker.Name.FullName())
+            //    .With(x => x.Phone = Faker.Phone.Number()).Build();
+
+            //context.Individuals.AddOrUpdate(x => x.Id, individuals.ToArray());
+
+            Category category = new Category() { Name = "Guitar" };
+            context.Categories.AddOrUpdate(x => x.Id, category);
+
+            context.Advertisements.AddOrUpdate(x => x.Id,
+                new Advertisement("Smart guitar", "Maerke1", "Model1", 1, "D1", "A1", "9000", "Aalborg", true, true, true, DateTime.Now, null, category)
+                );
+
+
         }
     }
 }
