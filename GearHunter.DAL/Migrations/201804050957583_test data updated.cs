efec86090b5e3fdea62d95d4d92b7e8e650ad0f6
Casper Froberg Andersen <casperfroberg@gmail.com>
2018-04-09 11:54:12 +0200

    Test data updated.

---
+++ GearHunter.DAL/Migrations/201804050957583_test data updated.cs
@@ -0,0 +1,16 @@
+namespace GearHunter.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class testdataupdated : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
