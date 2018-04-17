b75a48b563aeecd9e8b2faeb20a94223a3ff9d34
mikkellpaulsen <mikkellpaulsen@gmail.com>
2018-04-17 20:16:48 +0200

    GetByEmailTest

---
+++ GearHunter.Tests/BLL/IndividualFacadeTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GearHunter.BLL;
+using GearHunter.Core;
+
+namespace GearHunter.Tests.BLL
+{
+    [TestClass]
+    public class IndividualFacadeTest
+    {
+        private IndividualFacade individualFacade = new IndividualFacade();
+
+        [TestMethod]
+        public void GetByEmailTest()
+        {
+           // Individual Casper = new Individual("Casper Froberg", "DenDerVej 1", "9400", "Noerresundby", "Casper@fakemail.dk", "password1", "11111111", true, true, true)
+            Individual individual = individualFacade.GetByEmail("Casper@fakemail.dk");
+
+            Assert.IsNotNull(individual);
+            Assert.IsTrue(individual.IsActive);
+            Assert.AreEqual(individual.Name, "Casper Froberg");
+            Assert.AreEqual(individual.Email, "Casper@fakemail.dk");
+        }
+    }
+}
