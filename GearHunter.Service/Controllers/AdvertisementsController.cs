6b0446581624ebb23867da2a1b459c452b1281dd
CasperF <casperfroberg@gmail.com>
2018-04-19 00:25:26 +0200

    added  DELETE advertisement sample functionality

--- GearHunter.Service/Controllers/AdvertisementsController.cs
+++ GearHunter.Service/Controllers/AdvertisementsController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
@@ -9,6 +11,7 @@ using GearHunter.Core;
 
 namespace GearHunter.Service.Controllers
 {
+    //TODO - vi skal bruge models i vores webapi for validation.
     public class AdvertisementsController : ApiController
     {
         AdvertisementFacade advertisementFacade = new AdvertisementFacade();
@@ -17,7 +20,7 @@ namespace GearHunter.Service.Controllers
         public async Task<IEnumerable<Advertisement>> Get()
         {
             List<Advertisement> advertisements = await advertisementFacade.GetAdvertisementsAsync();
-            return advertisements;
+            //return advertisements;
         }
 
         // GET api/advertisements/5
@@ -27,8 +30,10 @@ namespace GearHunter.Service.Controllers
         }
 
         // POST api/advertisements
-        public void Post([FromBody]string value)
+        //Create a new advertisement.
+        public void Post([FromBody] Advertisement advertisement)
         {
+
         }
 
         // PUT api/advertisements/5
@@ -37,8 +42,39 @@ namespace GearHunter.Service.Controllers
         }
 
         // DELETE api/advertisements/5
-        public void Delete(int id)
+        public HttpResponseMessage Delete(int id)
         {
+            try
+            {
+                Advertisement tempAdvertisement = advertisementFacade.GetAdvertisement(id);
+
+                if(tempAdvertisement == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+                if(tempAdvertisement.IsActive == true)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "Can not delete Advertisement because it is currently active.");
+                }
+                //måske ikke nødvendigt med et kald mere til databasen her? Hvor vi henter den ovenover?
+                // kommer vel an på EF's måde at håndtere den på?
+                //++ skulle vi ændre så delete retunere true eller false afhængig af om det virker? 
+                //så kan vi bruge understående.
+
+                //if (advertisementFacade.DeleteAdvertisement(advertisementFacade.GetAdvertisement(id))
+                //{
+                //    Request.CreateResponse(HttpStatusCode.OK);
+                //}
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest);
+                }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, new AdvertisementWasNotDeletedException("The advertisement could not be deleted, please try again.", ex));
+            }
         }
     }
 }
---
+++ GearHunter.Service/Controllers/Exceptions/AdvertisementWasNotDeletedException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace GearHunter.Service.Controllers
+{
+    [Serializable]
+    internal class AdvertisementWasNotDeletedException : Exception
+    {
+        public AdvertisementWasNotDeletedException()
+        {
+        }
+
+        public AdvertisementWasNotDeletedException(string message) : base(message)
+        {
+        }
+
+        public AdvertisementWasNotDeletedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected AdvertisementWasNotDeletedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
