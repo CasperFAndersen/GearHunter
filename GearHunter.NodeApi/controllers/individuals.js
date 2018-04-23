bdb7c011bcc63a130c7c0cfa3d9124c1944c3532
Stefan Krabbe <stefankrabbe54@gmail.com>
2018-04-23 19:29:16 +0200

    Started making it possible to update, but it wont accept the request-body

--- GearHunter.NodeApi/controllers/individuals.js
+++ GearHunter.NodeApi/controllers/individuals.js
@@ -38,20 +38,20 @@ exports.create = (req, res, next) => {
   if (!req.body) return httpMsg.show500;
 
   let params = [];
-  params.push(new Parameter("Name", "NVarChar", req.body.Name));
-  params.push(new Parameter("Address", "NVarChar", req.body.Address));
-  params.push(new Parameter("Zip", "NVarChar", req.body.Zip));
-  params.push(new Parameter("City", "NVarChar", req.body.City));
-  params.push(new Parameter("Email", "NVarChar", req.body.Email));
-  params.push(new Parameter("Password", "NVarChar", req.body.Password));
-  params.push(new Parameter("Phone", "NVarChar", req.body.Phone));
+  params.push(new Parameter("name", "NVarChar", req.body.Name));
+  params.push(new Parameter("address", "NVarChar", req.body.Address));
+  params.push(new Parameter("zip", "NVarChar", req.body.Zip));
+  params.push(new Parameter("city", "NVarChar", req.body.City));
+  params.push(new Parameter("email", "NVarChar", req.body.Email));
+  params.push(new Parameter("password", "NVarChar", req.body.Password));
+  params.push(new Parameter("phone", "NVarChar", req.body.Phone));
   params.push(new Parameter("isAdmin", "Bit", 0));
   params.push(new Parameter("isActive", "Bit", 1));
   params.push(new Parameter("isValidated", "Bit", 0));
-  params.push(new Parameter("Discriminator", "NVarChar", "Individual"));
+  params.push(new Parameter("discriminator", "NVarChar", "Individual"));
 
   db.executeSql(
-    "INSERT INTO Users(name, address, zip, city, email, password, phone, isAdmin, isActive, IsValidated, Discriminator) VALUES ((@name), (@address), (@zip), (@city), (@email), (@password), (@phone), (@isAdmin), (@isActive), (@isValidated), (@Discriminator))",
+    "INSERT INTO Users(name, address, zip, city, email, password, phone, isAdmin, isActive, IsValidated, Discriminator) VALUES ((@name), (@address), (@zip), (@city), (@email), (@password), (@phone), (@isAdmin), (@isActive), (@isValidated), (@discriminator))",
     function(data, err) {
       if (err) {
         httpMsg.show500(req, res, err);
@@ -80,4 +80,47 @@ exports.delete = (req, res, next) => {
   );
 };
 
-exports.update = (req, res, next) => {};
+exports.update = (req, res, next) => {
+  if (!req.body) return httpMsg.show500;
+
+  let searchParams = [];
+  const idParam = new Parameter("id", "Int", req.params.id);
+  searchParams.push(idParam);
+
+  db.executeSql(
+    "select * from users where discriminator = 'individual' and id = (@id)",
+    function(data, err) {
+      if (err) {
+        httpMsg.show500(req, res, err);
+      }
+      if (Object.keys(data.recordset).length === 0) {
+        httpMsg.show404(req, res, next);
+      }
+      let updateParams = [];
+      updateParams.push(idParam);
+
+      let sqlStatement = "update users set";
+
+      if (req.body.Name) {
+        updateParams.push(new Parameter("name", "NVarChar", req.body.Name));
+        sqlStatement += " Name=(@name),";
+      }
+
+      sqlStatement = sqlStatement.slice(0, -1);
+      sqlStatement += " where id = (@id)";
+
+      db.executeSql(
+        sqlStatement,
+        function(data, err) {
+          if (err) {
+            httpMsg.show500(req, res, err);
+          } else {
+            httpMsg.show200(req, res, data, "Success");
+          }
+        },
+        updateParams
+      );
+    },
+    searchParams
+  );
+};
